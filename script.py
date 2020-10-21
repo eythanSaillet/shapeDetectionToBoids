6765bedee7ada4ca39fc241681c3952d87b8f92a
eythanSaillet <eythan.saillet@gmail.com>
2020-10-21 21:56:03 +0200

    :tada: init with shape detection and p5 base

---
+++ script.py
@@ -0,0 +1,69 @@
+from cv2 import cv2
+from p5 import *
+import numpy as np
+
+cam = cv2.VideoCapture(0)
+
+# Trackbar
+
+
+def nothing(x):
+    pass
+
+
+cv2.namedWindow("Trackbars")
+cv2.createTrackbar("L-H", "Trackbars", 0, 180, nothing)
+cv2.createTrackbar("L-S", "Trackbars", 147, 255, nothing)
+cv2.createTrackbar("L-V", "Trackbars", 133, 255, nothing)
+cv2.createTrackbar("U-H", "Trackbars", 180, 180, nothing)
+cv2.createTrackbar("U-S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U-V", "Trackbars", 255, 255, nothing)
+
+while True:
+    ret, frame = cam.read()
+
+    # Get trackbars values
+    LH = cv2.getTrackbarPos("L-H", "Trackbars")
+    LS = cv2.getTrackbarPos("L-S", "Trackbars")
+    LV = cv2.getTrackbarPos("L-V", "Trackbars")
+    UH = cv2.getTrackbarPos("U-H", "Trackbars")
+    US = cv2.getTrackbarPos("U-S", "Trackbars")
+    UV = cv2.getTrackbarPos("U-V", "Trackbars")
+
+    # Black detection try 1
+    # rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # lower = np.array([150, 150, 150])
+    # upper = np.array([255, 255, 255])
+    # mask = cv2.inRange(rgb, lower, upper)
+
+    # Red detection
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower = np.array([LH, LS, LV])
+    upper = np.array([UH, US, UV])
+    mask = cv2.inRange(hsv, lower, upper)
+    kernel = np.ones((10, 10), np.uint8)
+    mask = cv2.erode(mask, kernel)
+
+    # Contours detection
+    contours, _ = cv2.findContours(
+        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+
+        if area > 500:
+            cv2.drawContours(frame, [approx], 0, (0, 255, 0), 5)
+
+    cv2.imshow('Mask', mask)
+    cv2.imshow("Frame", frame)
+
+    k = cv2.waitKey(1)
+
+    if k == 27:
+        print("Escape hit, closing the app")
+        break
+
+cam.release()
+
+cam.destroyAllWindows()
---
+++ sketch/sketch.py
@@ -0,0 +1,15 @@
+from p5 import *
+
+
+def setup():
+    size(640, 360)
+
+
+def draw():
+    background('red')
+    fill('blue')
+    rect(0, 0, 100, 100)
+
+
+if __name__ == '__main__':
+    run()
