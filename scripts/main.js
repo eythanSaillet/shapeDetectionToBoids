12b36abb4228002c9af567a18da3eaee17d1f2a7
eythanSaillet <eythan.saillet@gmail.com>
2020-10-29 10:55:35 +0100

    :sparkles: wall drawing

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,6 @@ function setup() {
 	walls.push(new Wall(0, height - 0, width - 0, height - 0))
 	walls.push(new Wall(0, 0, 0, height - 0))
 	walls.push(new Wall(width - 0, 0, width - 0, height - 0))
-	walls.push(new Wall(width / 2, 150, width / 2, 450))
 
 	mouse = {
 		clickedPos: createVector(),
@@ -55,16 +54,7 @@ function draw() {
 
 		mouse.pos = createVector(mouseX, mouseY)
 
-		rect(mouse.clickedPos.x, mouse.clickedPos.y, mouse.pos.x - mouse.clickedPos.x, mouse.pos.y - mouse.clickedPos.y)
-		selectZone = [mouse.clickedPos.x, mouse.clickedPos.y, mouse.pos.x - mouse.clickedPos.x, mouse.pos.y - mouse.clickedPos.y]
-	}
-
-	// Draw active zone
-	for (const _zone of activeZone) {
-		// stroke('black')
-		// strokeWeight(1)
-		// noFill()
-		// rect(_zone[0], _zone[1], _zone[2], _zone[3])
+		line(mouse.clickedPos.x, mouse.clickedPos.y, mouse.pos.x, mouse.pos.y)
 	}
 }
 
@@ -74,14 +64,5 @@ function mousePressed() {
 
 function mouseReleased() {
 	activeZone.push(selectZone)
-	walls.push(new Wall(selectZone[0], selectZone[1], selectZone[2], selectZone[1]))
+	walls.push(new Wall(mouse.clickedPos.x, mouse.clickedPos.y, mouse.pos.x, mouse.pos.y))
 }
-
-// function mouseMoved() {
-// 	boids[0].vel.x = mouseX - width / 2
-// 	boids[0].vel.y = mouseY - height / 2
-
-// 	boids[0].vel.normalize()
-
-// 	console.log(intersectTest(walls[0], boids[0].pos, boids[0].vel))
-// }
